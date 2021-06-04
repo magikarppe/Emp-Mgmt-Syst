0cc8391f8e1739c3a5647bee826cc4829707e23c
Thalia De Mattos <tgdmattos@gmail.com>
2021-06-03 21:45:30 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// statements req
+
+// catabase connection
+
+// function init()
+
+// prompts 
+
+// Function update employees
+
+// 'add new employees' funct
+
+// 'add role' funct
+
+// 'add dept' funct 
+
